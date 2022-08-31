9e4f86a060e45f6335977d8600da4492ef519b9c
PaPond <s6303051613173@kmutnb.ac.th>
2022-08-31 11:26:17 +0700

    work5

---
+++ post.php
@@ -0,0 +1,25 @@
+้<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post</title>
+    <style type="text/css">
+        table th{text-align : left;background-color: #6CD2FE;}
+    </style>
+</head>
+<body>
+    <h1 style="text-align: center;">MY WEBBOARD</h1>
+    <hr>
+    <div align = center>
+    ต้องการดูกระทู้หมายเลข <?php echo $_GET["id"]; ?><br><br>
+    <table style = "border:2px solid black; width:40%"align ="center">
+           <tr><th colspan="2" >แสดงความคิดเห็น</th></tr>
+           <td><textarea name="comment"  cols="80" rows="10" align ="center"></textarea></td></tr> 
+           <tr><td colspan="2" align ="center"><input type="submit" value="ส่งข้อความ"></td></tr>        
+    </table>
+    <p style="text-align: center;"><a href="index2.html">กลับไปหน้าหลัก</a></p>
+    </div>
+</body>
+</html>
